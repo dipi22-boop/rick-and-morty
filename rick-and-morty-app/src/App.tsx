c97f9eab735dbf28525ea94aa5db559f8503a13e
dipi22-boop <66517018+dipi22-boop@users.noreply.github.com>
2025-06-15 15:13:40 +0000

    dynamic data from api response implemented

--- rick-and-morty-app/src/App.tsx
+++ rick-and-morty-app/src/App.tsx
@@ -1,10 +1,19 @@
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
 
+// --- Create a client for TanStack Query ---
+const queryClient = new QueryClient();
 function App() {
   return (
     <MainLayout>
-      <Home />
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider >
     </MainLayout>
   );
 }
